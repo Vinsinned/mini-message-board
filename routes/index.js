ed7dd5e4d89dc7faee463c6742a33d5fb14d9af7
vinson <dagger1245653@gmail.com>
2022-03-01 17:40:50 -0800

    finish with project (pretty easy n I forgot to save)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+/* GET new page */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Add a Message', heading: 'Enter your message!' });
+});
+router.post('/new', function (req, res, next) {
+  messages.push({text: req.body.text, user: req.body.name, added: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
